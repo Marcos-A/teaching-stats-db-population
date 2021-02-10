ad218e16fac6b1f47e5fe33e663eb81225ccc62d
Marcos-A <marcosalcocer@gmail.com>
2021-02-10 13:27:06 +0100

    First commit.

---
+++ README.md
@@ -0,0 +1,63 @@
+# school-form-db-population
+Populates the school-form database prior to submitting the form to the students to be answered.
+
+Designed as a helper to set up: [https://github.com/Marcos-A/school-form](https://github.com/Marcos-A/school-form)
+
+---
+
+### Requirements:
+1. Install:
+
+```
+sudo apt-get install python3	
+sudo apt-get install libpq-dev python-dev
+sudo apt install python3-pip
+pip3 install psycopg2
+```
+
+2. Set up your "database.ini" file and place it in the project's root folder:
+
+```
+[postgresql]
+host=YOUR-HOST
+database=YOUR-DATABASE
+user=YOUR-USER
+password=YOUR-PASSWORD
+port=YOUR-PORT
+options=-c search_path=dbo,YOUR-SCHEMA
+```
+
+3. Place your 4 data CSV files in the input folder following this pattern:
+
+cf_students.csv
+
+```
+header: ALUMNE,CORREU,GRUP,MP01,MP02,MP03,MP04,MP05,MP06,MP07,MP08,MP09,MP10,MP11,MP12,MP13,MP14,MP15
+
+MP columnes must contain 'X' to determine if the student is enrolled.
+```
+
+degrees.csv
+
+``` 
+header: Abreviatura,Nom,Família
+```
+
+eso-batx_students.csv
+
+``` 
+header: Adreça electrònica,Nom,Cognoms,Nivell,Grup
+```
+
+subjects.csv
+
+``` 
+header: MP,Nom,Cicle
+```
+
+---
+
+### How to run
+From within the project's root folder, run:
+
+`python3 insert_data.py`
---
+++ insert_data.py
@@ -0,0 +1,191 @@
+# -*- coding: UTF-8 -*-
+from config import config
+import csv
+import os
+import psycopg2
+import sys
+
+
+sys.path.append(os.path.dirname(__file__))
+CF_STUDENTS = 'input/cf_students.csv'
+DEGREES_FILE = 'input/degrees.csv'
+ESO_BATX_STUDENTS_FILE = 'input/eso-batx_students.csv'
+SUBJECTS_FILE = 'input/subjects.csv'
+
+
+def read_degrees(input_file):
+    degrees = ()
+    with open(input_file, 'r', encoding='utf-8') as degrees_file:
+        degrees_reader = csv.DictReader(degrees_file)
+
+        for degree in degrees_reader:
+            short_name = degree['Abreviatura']
+            full_name = degree['Nom']
+            department = degree['Família']
+
+            degrees += ((short_name, full_name, department,),)
+
+        return degrees
+
+
+def insert_degrees(degrees):
+    sql = """
+             INSERT INTO forms_degree(short_name, full_name, department)
+             VALUES(%s, %s, %s);
+          """
+    conn = None
+    try:
+        params = config()
+        print('Connecting to the PostgreSQL database...')
+
+        conn = psycopg2.connect(**params)
+        cursor = conn.cursor()
+
+        cursor.executemany(sql, degrees)
+
+        cursor.close()
+        conn.commit()
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+    finally:
+        if conn is not None:
+            conn.close()
+            print('Database connection closed.')
+
+
+def read_subjects(input_file):
+    subjects = ()
+    with open(input_file, 'r', encoding='utf-8') as subjects_file:
+        subjects_reader = csv.DictReader(subjects_file)
+
+        for subject in subjects_reader:
+            short_name = subject['MP']
+            full_name = subject['Nom']
+            degree = subject['Cicle']
+
+            subjects += ((short_name, full_name, degree,),)
+
+        return subjects
+
+
+def insert_subjects(subjects):
+    sql = """
+             INSERT INTO forms_subjects(short_name, full_name, degree_id)
+             VALUES(%s, %s, %s);
+          """
+    conn = None
+    try:
+        params = config()
+        print('Connecting to the PostgreSQL database...')
+
+        conn = psycopg2.connect(**params)
+        cursor = conn.cursor()
+
+        cursor.executemany(sql, subjects)
+
+        cursor.close()
+        conn.commit()
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+    finally:
+        if conn is not None:
+            conn.close()
+            print('Database connection closed.')
+
+
+def read_ESOBatx_students(input_file):
+    students = ()
+    with open(input_file, 'r', encoding='utf-8') as students_file:
+        students_reader = csv.DictReader(students_file)
+
+        for student in students_reader:
+            email = student['Adreça electrònica']
+            name = student['Nom']
+            surname = student['Cognoms']
+            level = student['Nivell']
+            classgroup = student['Grup']
+
+            students += ((email, name, surname, level, classgroup,),)
+
+        return students
+
+
+def insert_ESOBatx_students(students):
+    sql = """
+             INSERT INTO forms_enrolledstudent(email, name, surname,
+                                               level, classgroup)
+             VALUES(%s, %s, %s, %s, %s);
+          """
+    conn = None
+    try:
+        params = config()
+        print('Connecting to the PostgreSQL database...')
+
+        conn = psycopg2.connect(**params)
+        cursor = conn.cursor()
+
+        cursor.executemany(sql, students)
+
+        cursor.close()
+        conn.commit()
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+    finally:
+        if conn is not None:
+            conn.close()
+            print('Database connection closed.')
+
+
+def read_cf_students(input_file):
+    students = ()
+    with open(input_file, 'r', encoding='utf-8') as students_file:
+        students_reader = csv.DictReader(students_file)
+
+        for student in students_reader:
+            email = student['CORREU']
+            level = 'CF'
+            classgroup = student['GRUP']
+            enrolled_subjects = ','.join([key for key in student
+                                          if 'mp' in key.lower() and
+                                          student[key].lower() == 'x'])
+
+            students += ((email, level, classgroup, enrolled_subjects,),)
+
+        return students
+
+
+def insert_cf_students(students):
+    sql = """
+             INSERT INTO forms_enrolledstudent(email, level,
+                                               classgroup, enrolled_subjects)
+             VALUES(%s, %s, %s, %s);
+          """
+    conn = None
+    try:
+        params = config()
+        print('Connecting to the PostgreSQL database...')
+
+        conn = psycopg2.connect(**params)
+        cursor = conn.cursor()
+
+        cursor.executemany(sql, students)
+
+        cursor.close()
+        conn.commit()
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+    finally:
+        if conn is not None:
+            conn.close()
+            print('Database connection closed.')
+
+
+if __name__ == '__main__':
+    degrees = read_degrees(DEGREES_FILE)
+    insert_degrees(degrees)
+    subjects = read_subjects(SUBJECTS_FILE)
+    insert_subjects(subjects)
+    eso_batx_students = read_ESOBatx_students(ESO_BATX_STUDENTS_FILE)
+    insert_ESOBatx_students(eso_batx_students)
+    cf_students = read_cf_students(CF_STUDENTS)
+    insert_cf_students(cf_students)
